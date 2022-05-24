aa0b4cdedc74a030c560c78f2ce4b4566f18a31a
sheldoug <106186284+sheldoug@users.noreply.github.com>
2022-05-24 15:02:28 -0400

    Add files via upload

---
+++ multiples.java
@@ -0,0 +1,23 @@
+
+public class multiples {
+
+	public static void addNums(int x) {
+		int ans = 0, i = 0;
+		while (i != x) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				ans += i;
+//				System.out.println(i);
+			}
+			i++;
+			
+		}
+		System.out.println(ans);
+
+	}
+
+	public static void main(String[] args) {
+		addNums(10);
+
+	}
+
+}
